a1570e3f322686ce94a70bca65f81c0d3afce744
Rylie Castell <rylie.castell@gmail.com>
2023-05-04 23:16:16 +0200

    add js to answer button

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ cards.forEach((card) => {
   bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark-button--enabled");
   });
+  const answerButton = card.querySelector(
+    '[data-js="question-card--answer-button"'
+  );
+
+  answerButton.addEventListener("click", () => {
+    answerButton.classList.toggle("answer-button--enabled");
+  });
 });
