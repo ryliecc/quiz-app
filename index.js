1b4e262738f05e29aa6d6f0da9ec2e9aedd17abf
Rylie Castell <rylie.castell@gmail.com>
2023-05-05 22:54:16 +0200

    add textcontent for answer button if enabled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll('[data-js="question-card"]');
 
 cards.forEach((card) => {
-  const bookmarkButton = card.querySelector('[data-js="bookmark-button"');
+  const bookmarkButton = card.querySelector('[data-js="bookmark-button"]');
 
   bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark-button--enabled");
@@ -12,5 +12,14 @@ cards.forEach((card) => {
 
   answerButton.addEventListener("click", () => {
     answerButton.classList.toggle("answer-button--enabled");
+    if (answerButton.textContent == "Hide answer") {
+      answerButton.textContent = "Show answer";
+    } else {
+      answerButton.textContent = "Hide answer";
+    }
   });
+
+  const answerButtonEnabled = card.querySelector(
+    '[class="answer-button--enabled"]'
+  );
 });
