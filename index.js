9a08461a638293f64b8869283f219416fec6181b
Rylie Castell <rylie.castell@gmail.com>
2023-05-12 15:15:12 +0200

    remove unused code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ cards.forEach((card) => {
 
   bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark-button--enabled");
-    /* cards.classList.toggle("question-card--marked"); */
   });
   const answerButton = card.querySelector(
     '[data-js="question-card__answer-button"'
